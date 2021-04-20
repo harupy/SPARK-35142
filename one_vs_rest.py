63c93863a26134fea2e6bca62fbf4643d9d8f345
harupy <17039389+harupy@users.noreply.github.com>
2021-04-20 10:45:34 +0900

    test

---
+++ one_vs_rest.py
@@ -0,0 +1,20 @@
+from pyspark.ml.classification import LogisticRegression, OneVsRest
+from pyspark.ml.linalg import Vectors
+from pyspark.sql import SparkSession
+from sklearn.datasets import load_irisspark = SparkSession.builder.getOrCreate()
+
+X, y = load_iris(return_X_y=True)
+df = spark.createDataFrame(
+ [(Vectors.dense(features), int(label)) for features, label in zip(X, y)], ["features", "label"]
+)
+train, test = df.randomSplit([0.8, 0.2])lor = LogisticRegression(maxIter=5)
+ovr = OneVsRest(classifier=lor)ovrModel = ovr.fit(train)
+pred = ovrModel.transform(test)
+
+pred.printSchema()
+# This prints out:
+# root
+#  |-- features: vector (nullable = true)
+#  |-- label: long (nullable = true)
+#  |-- rawPrediction: string (nullable = true)  # <- should not be string
+#  |-- prediction: double (nullable = true)
